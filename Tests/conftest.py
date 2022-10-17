208a9d2a44f282c4655e8df200da24eeaa5e66f4
AtticusFinch1 <109745787+AtticusFinch1@users.noreply.github.com>
2022-10-17 16:52:54 +0400

    Update conftest.py

--- Tests/conftest.py
+++ Tests/conftest.py
@@ -4,8 +4,11 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
 from Config.config import TestData
+from pyvirtualdisplay import Display
 @pytest.fixture(autouse=True,scope='class')
 def init_driver(request): 
+    display = Display(visible=0, size=(800, 800))  
+    display.start()
     options = Options()
     options.add_argument('--headless')
     options.add_argument('--disable-gpu')
