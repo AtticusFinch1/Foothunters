a0e56b9e0d8c8eaa0ecf4a7a42e872a8e924a1d2
AtticusFinch1 <109745787+AtticusFinch1@users.noreply.github.com>
2022-10-18 21:12:31 +0400

    Update conftest.py

--- Tests/conftest.py
+++ Tests/conftest.py
@@ -10,8 +10,8 @@ from pyvirtualdisplay import Display
 @pytest.fixture(autouse=True, scope='class')
 def init_driver(request):
     """ display and --headless is used only for github actions """
-    # display = Display(visible=0, size=(800, 800))
-    # display.start()
+    display = Display(visible=0, size=(800, 800))
+    display.start()
     options = Options()
     options.add_argument('--headless')
     options.add_argument('--disable-gpu')
