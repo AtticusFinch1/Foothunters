a8378d85a7d7d4c947bd2f96328fc772d5d0e137
AtticusFinch1 <109745787+AtticusFinch1@users.noreply.github.com>
2022-12-05 10:49:35 +0400

    Update HomePage.py

--- Pages/HomePage.py
+++ Pages/HomePage.py
@@ -48,8 +48,8 @@ class HomePage(BasePage):
         BasePage.do_click(self, HomePage_locators.send_button)
 
     def check_message(self, rec, text_message):
-        time.sleep(10)
-        self.driver.get(TestData.BASE_URL + 'messenger')
+        time.sleep(5)
+        self.driver.get(TestData.BASE_URL + 'messenger/')
         print(self.driver.current_url)
         receivers_all = BasePage.do_find_elements(self, HomePage_locators.message_receiver)        
         receivers = [el.text for el in receivers_all]
