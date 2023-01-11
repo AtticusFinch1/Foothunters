5cd415f8479a467524731c23f9aca8f4e2246fa9
HovhannesTutyan <atutyanaz@gmail.com>
2023-01-11 11:25:17 +0400

    Registration fill complete bar final

--- Actions/RegisterPage.py
+++ Actions/RegisterPage.py
@@ -1,9 +1,13 @@
 from Pages.BasePage import BasePage
 from Config.config import TestData
+from Storage.path import GetPath
 from Locators.RegisterPage import Register_locators
-from dotenv import load_dotenv
-import os
+from Actions.GeneralActions import generalActions
+from selenium.webdriver.common.by import By
+from dotenv import load_dotenv 
+import os 
 import requests
+from time import sleep as s
 import json
 
 class ActionsPageRegister(BasePage):
@@ -12,3 +16,89 @@ class ActionsPageRegister(BasePage):
     def fill_fan_field(self, username, phone):
         self.do_send_keys(Register_locators.username_input, username)
         self.do_click(Register_locators.next_fan_finish)
+
+    def visit_profile(self):
+        self.do_click(Register_locators.avatar_img)
+        self.do_click(Register_locators.my_profile)
+
+    def fill_completeness_bar_profile(self):
+        data = generalActions.fake_data()
+        self.do_click(Register_locators.country_edit)
+        self.do_click(Register_locators.country_dropdown)
+        self.do_click(Register_locators.country_select)
+        self.do_send_keys(Register_locators.province, data['fake_name'])
+        self.do_send_keys(Register_locators.address_line, data['fake_address'][1:10])
+        self.do_send_keys(Register_locators.zip_code, data['fake_name'])
+        self.do_click(Register_locators.save_country)
+        s(5)
+        self.do_click(Register_locators.education_edit)
+        self.do_click(Register_locators.education_institution_dropdown)
+        self.do_click(Register_locators.institution_select)
+        self.do_send_keys(Register_locators.current_school, data['company_name'])
+        self.do_send_keys(Register_locators.current_club, data['company_name'])
+        self.do_send_keys(Register_locators.agent, data['company_name'])
+        self.do_click(Register_locators.save_education)
+        s(5)
+        self.driver.get(TestData.BASE_URL + "settings/details")
+        self.do_click(Register_locators.about_edit)
+        self.do_click(Register_locators.nationality_dropdown)
+        self.do_click(Register_locators.nationality_select)
+        self.do_click(Register_locators.gender_dropdown)
+        self.do_click(Register_locators.gender_select)
+        self.do_click(Register_locators.calendar_picker)
+        self.do_click(Register_locators.year_picker)
+        self.do_click(Register_locators.year)
+        self.do_click(Register_locators.year_apply)
+        self.do_send_keys(Register_locators.height, "180")
+        self.do_send_keys(Register_locators.weight, "80")
+        self.do_send_keys(Register_locators.description, data['company_name'])
+        self.do_click(Register_locators.save_about)
+        s(5)
+        self.do_click(Register_locators.position_edit)
+        self.do_click(Register_locators.pref_pos_dropdown)
+        self.do_click(Register_locators.pref_pos_select)
+        self.do_click(Register_locators.sec_pos_dropdown)
+        self.do_click(Register_locators.sec_pos_select)
+        self.do_click(Register_locators.pref_foot_dropdown)
+        self.do_click(Register_locators.pref_foot_select)
+        self.do_click(Register_locators.save_position)
+
+    def fill_completeness_bar_avatar(self):
+        self.do_click(Register_locators.edit_profile_photo)
+        self.do_click(Register_locators.update_profile_photo)
+        s(5) # while the element can not be visible
+        waitless = self.driver.find_element(By.XPATH, "//input[@type='file']")
+        s(5)
+        waitless.send_keys(GetPath.file_example_global('avatar.jpg'))
+        s(5)
+        self.do_click(Register_locators.upload_btn)
+
+    def fill_completeness_bar_cover(self):
+        self.do_click(Register_locators.edit_cover_photo)
+        self.do_click(Register_locators.update_cover_photo)
+        s(5) # while the element can not be visible
+        waitless = self.driver.find_element(By.XPATH, "//input[@type='file']")
+        s(5)
+        waitless.send_keys(GetPath.file_example_global('cover.jpg'))
+        s(15)
+        self.do_click(Register_locators.upload_btn)
+    
+    def fill_completeness_bar_photo(self):
+        self.do_click(Register_locators.photo_upload_btn)
+        waitless = self.driver.find_element(By.XPATH, "//input[@type='file']")
+        s(5)
+        waitless.send_keys(GetPath.file_example_global('cover.jpg'))
+        s(5)
+        self.do_click(Register_locators.upload_btn_photo)
+
+    def fill_completeness_bar_video(self):
+        self.scroll_down(1)
+        self.do_click(Register_locators.video_upload_btn)
+        waitless = self.driver.find_element(By.XPATH, "//input[@type='file']")
+        s(5)
+        waitless.send_keys(GetPath.file_example_global('video.mp4'))
+        s(15)
+        self.do_send_keys(Register_locators.video_title_input, "video title") 
+        self.do_send_keys(Register_locators.video_description_input, "video description")
+        s(2)
+        self.do_click(Register_locators.video_publish_btn)
--- Tests/test_RegisterPage.py
+++ Tests/test_RegisterPage.py
@@ -1,10 +1,14 @@
 from Tests.test_base import BaseTest
 from Pages.RegisterPage import RegisterPage
+from Pages.LoginPage import LoginPage
 from Tests.conftest import TestData
 from Pages.BasePage import BasePage
 from Pages.GiftsPage import GiftsPage
+from Locators.HomePage import HomePage_locators
 from Actions.GeneralActions import generalActions
+from Actions.RegisterPage import ActionsPageRegister
 from googleApi import GoogleApi
+from time import sleep as s
 import pytest
 import requests
 import time
@@ -13,29 +17,65 @@ import os
 class Test_RegisterPage(BaseTest):
     @pytest.mark.parametrize('age, role, expected', 
     [
-        pytest.param('21', 'Manager', '10'),
-        pytest.param('23', 'Coach', '10'),
-        pytest.param('16', 'Player', '10'),
-        pytest.param('22', 'CEO', '10'),
-        pytest.param('22', 'Agent', '10'),
+        pytest.param(21, 'Manager', '10'),
+        pytest.param(10, 'Player', '10'),
+        pytest.param(23, 'Coach', '10'),
+        pytest.param(16, 'Player', '10'),
+        pytest.param(22, 'CEO', '10'),
+        pytest.param(22, 'Agent', '10'),
+        pytest.param(10, 'Fan', '10')
     ])
     def test_valid_register_global(self, age, role, expected):
         self.registerPage = RegisterPage(self.driver)
         self.registerPage.select_role(age, role)
         request = generalActions.create_new_email()
-        first_name = request['first_name']
-        last_name = request['last_name']
-        email = os.getenv("GOOGLE_MAIL")
-        password = os.getenv("PASSWORD")
-        self.registerPage.valid_register_general(first_name, last_name, email, password)
+        if age < 13 and role == 'Player':
+            parent_first_name = request['first_name']
+            last_name = request['last_name']
+            kid_first_name = parent_first_name + 'sKid'
+            email = os.getenv("GOOGLE_MAIL")
+            password = os.getenv("PASSWORD")
+            self.registerPage.valid_register_parental(parent_first_name, last_name, kid_first_name, email, password)
+        else:
+            first_name = request['first_name']
+            last_name = request['last_name']
+            email = os.getenv("GOOGLE_MAIL")
+            password = os.getenv("PASSWORD")
+            self.registerPage.valid_register_general(first_name, last_name, email, password)
         googleapi = GoogleApi()
         service = googleapi.gmail_authenticate()
         results = googleapi.search_messages(service, "FootHunters")
-        print(f"Found {len(results)} results.")
         letter = googleapi.read_message(service, results[0])
-        print('letter+++++', letter)
         self.driver.get(letter)
-        time.sleep(2)
+        s(2)
         points = GiftsPage.check_points(self)
         assert points["banner_points"] == expected and points["card_points"] == expected
         self.registerPage.delete_account()
+
+    def test_fill_completeness_bar(self):
+        self.registerPage = RegisterPage(self.driver)
+        self.actionsPageRegister = ActionsPageRegister(self.driver)
+        self.registerPage.select_role(20, 'Player')
+        request = generalActions.create_new_email()
+        first_name = request['first_name']
+        last_name = request['last_name']
+        email = request['email']
+        password = os.getenv("PASSWORD")
+        self.registerPage.valid_register_general( first_name, last_name, email, password )
+        inbox = generalActions.get_inbox()
+        self.driver.get(inbox[0])
+        BasePage.wait_for_page_load( self, HomePage_locators.precense_of_home_page_el )
+        result1 = self.registerPage.fill_completeness_bar_profile()
+        assert result1['progress_initial'] == 'width: 16.6667%;' and result1['progress_second'] == 'width: 33.3333%;'
+        s(2)
+        result2 = self.registerPage.upload_fill_complit_bar(self.actionsPageRegister.fill_completeness_bar_avatar)
+        assert result2['progress_global'] == 'width: 33.3333%;'
+        s(2)
+        result3 = self.registerPage.upload_fill_complit_bar(self.actionsPageRegister.fill_completeness_bar_cover)
+        assert result3['progress_global'] == 'width: 50%;'
+        s(2)
+        result4 = self.registerPage.upload_fill_complit_bar(self.actionsPageRegister.fill_completeness_bar_photo)
+        assert result4['progress_global'] == 'width: 66.6667%;'
+        s(2)
+        result5 = self.registerPage.upload_fill_complit_bar(self.actionsPageRegister.fill_completeness_bar_video)
+        assert result5['progress_global'] == 'width: 83.3333%'
